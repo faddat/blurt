af20889a9df461b14dd92df5a5e1051ae53ff19d
Jacob Gadikian <jacobgadikian@gmail.com>
2021-01-06 15:54:10 +0700

    Stuff to support blog posts.  Lacks reward pool and fees and some oter stuff.

---
+++ x/blurt/client/cli/txPost.go
@@ -0,0 +1,95 @@
+package cli
+
+import (
+  
+	"github.com/spf13/cobra"
+
+    "github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/faddat/blurt/x/blurt/types"
+)
+
+func CmdCreatePost() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "create-post [title] [body]",
+		Short: "Creates a new post",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+      argsTitle := string(args[0])
+      argsBody := string(args[1])
+      
+        	clientCtx := client.GetClientContextFromCmd(cmd)
+			clientCtx, err := client.ReadTxCommandFlags(clientCtx, cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgCreatePost(clientCtx.GetFromAddress().String(), string(argsTitle), string(argsBody))
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+    return cmd
+}
+
+func CmdUpdatePost() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "update-post [id] [title] [body]",
+		Short: "Update a post",
+		Args:  cobra.ExactArgs(3),
+		RunE: func(cmd *cobra.Command, args []string) error {
+            id := args[0]
+      argsTitle := string(args[1])
+      argsBody := string(args[2])
+      
+        	clientCtx := client.GetClientContextFromCmd(cmd)
+			clientCtx, err := client.ReadTxCommandFlags(clientCtx, cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgUpdatePost(clientCtx.GetFromAddress().String(), id, string(argsTitle), string(argsBody))
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+    return cmd
+}
+
+func CmdDeletePost() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "delete-post [id] [title] [body]",
+		Short: "Delete a post by id",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+            id := args[0]
+
+        	clientCtx := client.GetClientContextFromCmd(cmd)
+			clientCtx, err := client.ReadTxCommandFlags(clientCtx, cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgDeletePost(clientCtx.GetFromAddress().String(), id)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+    return cmd
+}
---
+++ x/blurt/handler_post.go
@@ -0,0 +1,44 @@
+package blurt
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+    sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/faddat/blurt/x/blurt/types"
+	"github.com/faddat/blurt/x/blurt/keeper"
+)
+
+func handleMsgCreatePost(ctx sdk.Context, k keeper.Keeper, msg *types.MsgCreatePost) (*sdk.Result, error) {
+	k.CreatePost(ctx, *msg)
+
+	return &sdk.Result{Events: ctx.EventManager().ABCIEvents()}, nil
+}
+
+func handleMsgUpdatePost(ctx sdk.Context, k keeper.Keeper, msg *types.MsgUpdatePost) (*sdk.Result, error) {
+	var post = types.Post{
+		Creator: msg.Creator,
+		Id:      msg.Id,
+    	Title: msg.Title,
+    	Body: msg.Body,
+	}
+
+    if msg.Creator != k.GetPostOwner(ctx, msg.Id) { // Checks if the the msg sender is the same as the current owner
+        return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Incorrect owner") // If not, throw an error                                                                                             
+    }          
+
+	k.UpdatePost(ctx, post)
+
+	return &sdk.Result{Events: ctx.EventManager().ABCIEvents()}, nil
+}
+
+func handleMsgDeletePost(ctx sdk.Context, k keeper.Keeper, msg *types.MsgDeletePost) (*sdk.Result, error) {
+    if !k.HasPost(ctx, msg.Id) {                                                                                                                                                                    
+        return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, msg.Id)                                                                                                                                
+    }                                                                                                                                                                                                  
+    if msg.Creator != k.GetPostOwner(ctx, msg.Id) {
+        return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "Incorrect owner")                                                                                                                       
+    } 
+
+	k.DeletePost(ctx, msg.Id)
+
+	return &sdk.Result{Events: ctx.EventManager().ABCIEvents()}, nil
+}
